c0a08d4016a27298fa6960d6e1abe63199122c2f
Yuri Sanford <yurirodriguessanford@gmail.com>
2023-06-02 22:26:25 -0300

    Criando metodo para delete

--- src/app.js
+++ src/app.js
@@ -32,6 +32,18 @@ app.put('/biceps/:id', (req, res) => {
     res.status(200).json(exercicios);
 });
 
+app.get('/biceps/:id', (req, res) => {
+    const index = buscaExercicio('Biceps', req.params.id);
+    res.status(200).json(exercicios[index]);
+});
+
+app.delete('/biceps/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaExercicio('Biceps', id);
+    exercicios.splice(index, 1);
+    res.send('Exercício ' + id + ' excluído com sucesso');
+});
+
 function buscaExercicio(grupo, id) {
     const ex = exercicios.filter((exercicio) => exercicio.grupo === grupo);
     return ex.findIndex((ex) => ex.id == id);
